98c3f61a5a0c6e402ee02c10e152f96b5d265cc8
Hemanth <apple@Hemanths-MacBook-Pro-2.local>
2026-03-08 15:32:23 +0530

    backend vercel.json

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const api = axios.create({
-    baseURL: '/api',
+    baseURL: process.env.REACT_APP_API_URL || '/api',
 });
 
 // Add a request interceptor to include the auth token
