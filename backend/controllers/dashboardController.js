74b2463e112d4e42a02800294cac848a9e7590f3
Hemanth <apple@Hemanths-MacBook-Pro-2.local>
2026-03-09 19:59:02 +0530

    css changes and added fields

--- backend/controllers/dashboardController.js
+++ backend/controllers/dashboardController.js
@@ -58,7 +58,7 @@ const getDashboardSummary = async (req, res) => {
         });
     } catch (error) {
         res.status(500).json({ message: error.message });
-    }
+    } 
 };
 
 const getDashboardCharts = async (req, res) => {
